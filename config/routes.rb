564bc8192ff8aee2e19650aef422d144032e6c05
Aristotelis Christou <telischristou@gmail.com>
2018-12-17 23:40:14 +0200

    Make json the default request format

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #
-  resources :authors do
+  scope '/', defaults: { format: 'json' } do
+    resources :authors do
+      resources :books
+    end
+
+    resources :publishers do
+      resources :books
+    end
+
     resources :books
   end
-
-  resources :publishers do
-    resources :books
-  end
-
-  resources :books
 end
