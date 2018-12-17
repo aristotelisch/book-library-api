1b6e921aa20e9a6bedac5e40634116119efe8e3e
Aristotelis Christou <telischristou@gmail.com>
2018-12-17 22:49:40 +0200

    Add model methods

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,8 @@
 class Author < ApplicationRecord
   has_many :books
   has_many :publishers, through: :books
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,28 @@
 class Book < ApplicationRecord
   belongs_to :publisher
   belongs_to :author
+
+  def truncated_description
+    description.length > 100 ? description[0, 100] + '...' : description
+  end
+
+  def author_full_name
+    author.full_name
+  end
+
+  def author_email
+    author.email
+  end
+
+  def author_date_of_birth
+      author.date_of_birth
+  end
+
+  def publisher_name
+      publisher.name
+  end
+
+  def publisher_address
+      publisher.address
+  end
 end
